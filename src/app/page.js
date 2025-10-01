6a0f055a13a074b5d67280170ca743ff7389d20c
fikrifauzi227 <fikrifauzi227@gmail.com>
2025-10-01 14:51:52 +0700

    homepage-animasi

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,16 @@
 
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, EffectCreative } from "swiper/modules";
+import { Autoplay, EffectFade } from "swiper/modules"; // ✅ pakai EffectFade
 import "swiper/css";
-import "swiper/css/effect-creative";
+import "swiper/css/effect-fade"; // ✅ import CSS fade
 
 export default function Home() {
   const wisataSlides = [
     { img: "/IMG_6806 1.png", alt: "Pantai" },
     { img: "/IMG_5702 2.png", alt: "Gunung" },
     { img: "/IMG_7236 3.png", alt: "Air Terjun" },
+    { img: "/IMG3.png", alt: "Air Terjun" },
   ];
 
   const kulinerSlides = [
@@ -21,26 +22,35 @@ export default function Home() {
 
   return (
     <main>
-      {/* HERO SLIDER DENGAN EFFECT CREATIVE */}
+            {/* NAVBAR */}
+      <header className="navbar">
+        <div className="logo">SIDimas.</div>
+        <nav>
+          <ul>
+            <li><a href="#">Beranda</a></li>
+            <li><a href="#">Wisata</a></li>
+            <li><a href="#">Kuliner</a></li>
+            <li><a href="#">Budaya</a></li>
+            <li><a href="#">Kontribusi</a></li>
+          </ul>
+        </nav>
+        <div className="nav-actions">
+          <a href="#" className="login">Log in</a>
+          <a href="#" className="signup">Sign Up</a>
+        </div>
+      </header>
+
+      {/* HERO SLIDER */}
       <Swiper
-        modules={[EffectCreative, Autoplay]}
-        effect="creative"
-        creativeEffect={{
-          prev: {
-            shadow: true,
-            translate: ["-120%", 0, -500], // jauh geser keluar layar
-            rotate: [0, 0, -90], // diputer biar keliatan dramatis
-          },
-          next: {
-            shadow: true,
-            translate: ["120%", 0, -500],
-            rotate: [0, 0, 90],
-          },
-        }}
-        loop={true}
-        autoplay={{ delay: 5000, disableOnInteraction: false }}
-        className="hero-slider"
-      >
+  modules={[EffectFade, Autoplay]}
+  effect="fade"
+  fadeEffect={{ crossFade: true }}
+  loop={true}
+  speed={2500} // ✅ durasi transisi fade (ms) -> 1.5 detik
+  autoplay={{ delay: 5000, disableOnInteraction: false }}
+  className="hero-slider"
+>
+
         {/* SLIDE 1: WISATA */}
         <SwiperSlide>
           <section
